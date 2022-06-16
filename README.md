5f1c4bcafeb660abef3b40d1bd62c6ea39f193dd
Tony Malkowski <tony@txstate.edu>
2022-06-16 06:04:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Usage:
+```
+cp netbox-cli.init.sh.example netbox-cli.init.sh
+# edit netbox-cli.init.sh and set netbox hostname + api token
+
+# then, to use:
+source netbox-cli.init.sh
+
+```
+
+Either do that in your .bash_profile, or just source it when you want to use it in that shell.
+
---
+++ netbox-cli.sh
@@ -0,0 +1,112 @@
+# this file is sourced from netbox-cli.init.sh
+
+# main function: this is what we will run on the command line
+netbox () {
+	subcmd="$1"
+	shift
+
+	case "$subcmd" in
+		get)
+			__netbox_get "$@"
+		;;
+		post)
+			__netbox_post "$@"
+		;;
+		put)
+			__netbox_put "$@"
+		;;
+		patch)
+			__netbox_patch "$@"
+		;;
+		delete)
+			__netbox_delete "$@"
+		;;
+	esac | jq
+}
+
+__netbox_build_path () {
+
+	path="$1"
+	args="$2"
+
+	if echo "$path" | grep -qF '?'; then
+		if [[ ! -z "$args" ]]; then
+			echo "Error: cannot include ? in first arg if also including second arg" >&2
+			return
+		fi
+	elif ! echo "$path" | grep -qE '/$'; then
+		path="$path/"
+	fi
+
+	if [[ -z "$args" ]]; then
+		args=""
+	elif echo "$args" | grep -qE '^[0-9]+$'; then
+		args="$args/"
+	else
+		args="?$args"
+	fi
+	echo "https://$__netbox_hostname/api$path$args"
+}
+
+__netbox_curl () {
+	curl -s -H "Authorization: Token $__netbox_token" -H 'Content-Type: application/json' "$@"
+}
+
+__netbox_get () { 
+	__netbox_curl -X GET "$( __netbox_build_path "$1" "$2" )"
+}
+
+__netbox_post () {
+	__netbox_curl -X POST --data-raw "$2" "$( __netbox_build_path "$1" )"
+}
+
+__netbox_put () {
+	__netbox_curl -X PUT --data-raw "$2" "$( __netbox_build_path "$1" )"
+}
+
+__netbox_patch () {
+	__netbox_curl -X PATCH --data-raw "$2" "$( __netbox_build_path "$1" )"
+}
+
+__netbox_delete () {
+	__netbox_curl -X DELETE "$( __netbox_build_path "$1" "$2" )" 
+}
+
+function _sed () {
+	if which gsed 2>/dev/null; then
+		gsed "$@"
+	else
+		sed "$@"
+	fi
+}
+
+# rebuild autocomplete file
+if [[ ! -e "$__netbox_completion_cache" ]] || [[ $(( $( date +%s ) - $( stat -f %m "$__netbox_completion_cache" ) )) -gt 86400 ]]; then
+	netbox get / | jq | grep http | cut -d\" -f2 | while read path; do
+		netbox get /$path/ | grep http | _sed -re 's|.*/api/|/|' -e 's|/".*|/|';
+	done > "$__netbox_completion_cache"
+fi
+
+function __netbox_bash_completion () {
+
+    local cmd="${1##*/}"
+    local word=${COMP_WORDS[COMP_CWORD]}
+    local line=${COMP_LINE}
+
+	local subcmd=$( echo "$line" | awk '{ print $2 }' )
+	local api_path=$( echo "$line" | awk '{ print $3 }' )
+	local api_args=$( echo "$line" | awk '{ print $4 }' )
+
+	if [[ "$subcmd" == "$word" ]]; then
+		COMPREPLY=($( printf "get\npost\nput\npatch\ndelete\n" | grep -E "^$word" ))
+	elif [[ "$api_path" == "$word" ]]; then
+		COMPREPLY=($( grep -E "^$word" "$__netbox_completion_cache" ))
+	#elif [[ "$api_args" == "$word" ]]; then
+	else
+		COMPREPLY=()
+	fi
+
+}
+
+complete -F __netbox_bash_completion netbox
+
