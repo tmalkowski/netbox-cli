d67e6cd8e7adb3eb9cdd6184b4fd208247caf754
su-t_m170 <t_m170@txstate.edu>
2022-06-16 11:53:43 -0500

    netbox-cli debugging in RHEL8

--- netbox-cli/netbox-cli.sh
+++ netbox-cli/netbox-cli.sh
@@ -1,17 +1,18 @@
 # this file is sourced from netbox-cli.init.sh
 
-# if we have jq installed, use it. otherwise, fall back to json_xs, json_pp, or just plain cat
-_jq(){
-	if which jq 2>/dev/null >/dev/null; then
-		jq
-	elif which json_pp 2>/dev/null >/dev/null; then
-		json_pp
-	elif which json_xs 2>/dev/null >/dev/null; then
-		json_xs
-	elif which cat 2>/dev/null >/dev/null; then
-		cat
-	fi
-}
+if ! which json_pp 2>/dev/null >/dev/null; then
+	echo "ERROR: missing json_pp" >&2
+	echo "Might try: dnf whatprovides '*/json_pp'" >&2
+	echo >&2
+	echo "(or if you're using non-system perl, try cpanm JSON::PP)" >&2
+	echo >&2
+	return
+fi
+
+# if we have jq installed, use it. otherwise, fall back to boring greyscale
+if ! which jq 2>/dev/null >/dev/null; then
+	alias jq=json_pp
+fi
 
 _stat(){
 	if stat --version 2>/dev/null >/dev/null; then
@@ -29,23 +30,31 @@ netbox () {
 	subcmd="$1"
 	shift
 
+	out=$(mktemp)
 	case "$subcmd" in
 		get)
-			__netbox_get "$@"
+			__netbox_get "$@" >"$out"
 		;;
 		post)
-			__netbox_post "$@"
+			__netbox_post "$@" >"$out"
 		;;
 		put)
-			__netbox_put "$@"
+			__netbox_put "$@" >"$out"
 		;;
 		patch)
-			__netbox_patch "$@"
+			__netbox_patch "$@" >"$out"
 		;;
 		delete)
-			__netbox_delete "$@"
+			__netbox_delete "$@" >"$out"
 		;;
-	esac | _jq
+	esac
+
+	if [[ -t 1 ]]; then
+		cat "$out" | jq
+	else
+		cat "$out" | json_pp
+	fi
+	rm -f "$out" >/dev/null 2>/dev/null
 }
 
 __netbox_build_path () {
@@ -105,8 +114,7 @@ function _sed () {
 }
 
 # rebuild autocomplete file
-if [[ ! -e "$__netbox_completion_cache" ]] || [[ $(( $( date +%s ) - $( _stat "$__netbox_completion_cache" ) )) -gt 86400 ]]; then
-
+if [[ ! -e "$__netbox_completion_cache" ]] || [[ ! -s "$__netbox_completion_cache" ]] || [[ $(( $( date +%s ) - $( _stat "$__netbox_completion_cache" ) )) -gt 86400 ]]; then
 
 	# first, let's make sure we can talk to the API
 	# (if the cache is already present, it's reasonable to assume we have API access)
@@ -121,9 +129,9 @@ if [[ ! -e "$__netbox_completion_cache" ]] || [[ $(( $( date +%s ) - $( _stat "$
 	rm -f "$headers" 2>/dev/null >/dev/null
 	unset headers
 
-
-	netbox get / | _jq | grep http | cut -d\" -f2 | while read path; do
-		netbox get /$path/ | grep http | _sed -re 's|.*/api/|/|' -e 's|/".*|/|';
+	# now let's rebuild the cache
+	__netbox_get / | json_pp | grep http | cut -d\" -f2 | while read path; do
+		__netbox_get /$path/ | json_pp | grep http | _sed -re 's|.*/api/|/|' -e 's|/".*|/|';
 	done > "$__netbox_completion_cache"
 fi
 
